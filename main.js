0b4a4dd97527f09b9602239feaa86193f9c5d103
Noah Roston <rostonn@gmail.com>
2015-11-12 14:29:46 -0700

    Changed to download images and made them circles

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-$.get('https://www.reddit.com/r/aww.json', function(data){
-  for (i=0;i<data.data.children.length; i++){
-    console.log(data.data.children[i]);
-  }
-
+$(function(){
+    $.get('https://www.reddit.com/r/aww.json', function(data){
+        var $imageArea = $('#imageArea');
+        for (i=0;i<data.data.children.length; i++){
+          console.log(data.data.children[i].data.thumbnail);
+            $imageArea.append('<img src="'+data.data.children[i].data.thumbnail+'">');
+        }
+      })
 })
