06a61c4788ded24da2302b07025ff42ecd1cf0af
J. Drill <JJDrill@hotmail.com>
2015-11-12 14:51:16 -0700

    J's changes

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 $(function(){
-    $.get('https://www.reddit.com/r/aww.json', function(data){
-        var $imageArea = $('#imageArea');
-        for (i=0;i<data.data.children.length; i++){
-          console.log(data.data.children[i].data.thumbnail);
-            $imageArea.append('<img src="'+data.data.children[i].data.thumbnail+'">');
-        }
-      })
+  $.get('https://www.reddit.com/r/awwjjjj.json')
+  .done(function(data){
+      localStorage.setItem('awwData', JSON.stringify(data));
+      appendImagesToMain(data);
+  })
+  .fail(function(){
+    console.log('Failed!');
+    var offLineData = JSON.parse( localStorage.getItem('awwData') );
+    appendImagesToMain(offLineData);
+  })
+
+  appendImagesToMain = function(data){
+    var $imageArea = $('#imageArea');
+    for (i=0;i<data.data.children.length; i++){
+      console.log(data.data.children[i].data.thumbnail);
+        $imageArea.append('<img src="'+data.data.children[i].data.thumbnail+'">');
+    }
+  }
 })
